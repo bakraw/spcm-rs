ddaefe047fe738d216fd1bc40558bc2eb55a6792
bakraw <161661948+bakraw@users.noreply.github.com>
2026-06-12 09:31:48 +0000

    utils mod + removed buffer type genericity

--- src/lib.rs
+++ src/lib.rs
@@ -3,43 +3,9 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
 
+pub mod core {
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-use std::{ffi, os::raw, ptr};
-
-/*****
-* 4096-aligned buffer to use for data transfers.
-* Sadly align() has to be hardcoded so I settled for the standard 4 KiB,
-* modify that number if you need bigger alignement for whatever reason.
-* Instantiate on the heap with:
-* let mut buf = Box::new(PageAlignedBuffer<{type}, {size}>(0, {size})) 
-******/
-#[repr(align(4096))]
-pub struct PageAlignedBuffer<T, const N: usize>(pub [T;N]);
-
-/*****
- * Returns a void* to a PageAlignedBuffer.
- * If the buffer is in a box (heap allocatad), you need to dereference it.
- *****/
-pub fn get_buf_raw_ptr<T, const N: usize>(buf: &mut PageAlignedBuffer<T, N>) -> *mut raw::c_void {
-	buf as *const PageAlignedBuffer<T, N> as *mut raw::c_void
 }
 
-/*****
- * Reads the specified card's error string, and returns it as Some(String) if
- * it's not null (else None).
- * I initially tried to get it to return Some(&str), but the conversion required
- * UTF-8 while the SDK returns ASCII. At least the heap allocation only happens
- * when an error occurs (at which point you'd probably want to kill the program
- * anyway).
- *****/
-pub fn get_error(h_device: drv_handle) -> Option<String> {
-	let mut err_text_buf = [0u8; ERRORTEXTLEN as usize];
-	let err_text_ptr: *mut i8 = err_text_buf.as_mut_ptr() as *mut ffi::c_char;
-	if unsafe{spcm_dwGetErrorInfo_i64(
-				h_device, ptr::null_mut(), ptr::null_mut(), err_text_ptr)} != 0 {
-		return Some(unsafe{ffi::CStr::from_ptr(err_text_ptr)}.to_string_lossy().into_owned());
-	}
-	
-	None
-}
+pub mod utils;
+pub use core::*;
---
+++ src/utils.rs
@@ -0,0 +1,69 @@
+
+use {crate::core,
+	 std::{ffi, os::raw, ptr, slice}};
+
+/// * 4096-aligned buffer to use for data transfers.
+/// * ``N`` is the size of the buffer in bytes.
+/// * Sadly align() has to be hardcoded so I settled for the standard 4 KiB page size,
+///		modify that number if a different page size is used.
+/// * Instantiate on the heap with:
+///		```rust 
+/// 		let mut buf = Box::new(PageAlignedBuffer</*size*/>(0, /*size*/));
+/// 	``` 
+#[repr(align(4096))]
+pub struct PageAlignedBuffer<const N: usize>(pub [u8;N]);
+
+impl<const N: usize> PageAlignedBuffer<N> {
+	/// * Use to read the buffer if the card is in 16-bits unpacked samples mode.
+	/// * Negligible performance overhead (effectively just constructs a fat pointer).
+	/// * _**If N is uneven, the last byte will be ignored**_ (which is most likely
+	/// 	not what you want).
+	pub fn as_i16_samples(&self) -> &[i16] {
+		// Unsafe block, but casting a pair of u8 to a i16 is memory-safe if the buffer
+		// has proper 2-bytes alignment (which it has in our case, as it's page-aligned).
+		// Even if N % 2 != 0, N / 2 will round down, such that the last u8 get ignored.
+		unsafe {
+			slice::from_raw_parts(self.0.as_ptr() as *const i16, N/2)
+		}
+	}
+
+	/// * Use to read the buffer if the card is in 8-bits packed samples mode.
+	/// * Negligible performance overhead (effectively just constructs a fat pointer).
+	pub fn as_i8_samples(&self) -> &[i8] {
+		// Unsafe block, but casting a u8 to a i8 is memory-safe.
+		unsafe {
+			slice::from_raw_parts(self.0.as_ptr() as *const i8, N)
+		}
+	}
+
+	// TODO: as_i12_samples()
+}
+
+
+/// * Returns a void* to a given ``PageAlignedBuffer``.
+/// * If the buffer is in a box (heap allocated, which it usually is), you need to dereference it
+/// 	when passing it to the function.
+/// 	```rust
+/// 		let	buffer_ptr: *mut raw::c_void = spcm::get_buf_raw_ptr(&mut *buffer);
+/// 	```
+pub fn get_buf_raw_ptr<const N: usize>(buf: &mut PageAlignedBuffer<N>) -> *mut raw::c_void {
+	buf as *const PageAlignedBuffer<N> as *mut raw::c_void
+}
+
+/// * Reads the specified card's error string, and returns it as ``Some(String)`` if
+///		it's not null (else ``None``).
+/// * I initially tried to get it to return ``Some(&str)``, but the conversion required
+/// 	UTF-8 while the SDK returns bytes usually interpreted as ASCII. Getting a proper
+/// 	conversion seems to be a PITA and would increase complexity a bunch. At least the
+/// 	heap allocation only happens when an error occurs (at which point you'd probably
+/// 	want to kill the program anyway).
+pub fn get_error(device: core::drv_handle) -> Option<String> {
+	let mut err_text_buf = [0u8; core::ERRORTEXTLEN as usize];
+	let err_text_ptr: *mut i8 = err_text_buf.as_mut_ptr() as *mut ffi::c_char;
+	if unsafe{core::spcm_dwGetErrorInfo_i64(
+				device, ptr::null_mut(), ptr::null_mut(), err_text_ptr)} != 0 {
+		return Some(unsafe{ffi::CStr::from_ptr(err_text_ptr)}.to_string_lossy().into_owned());
+	}
+	
+	None
+}
